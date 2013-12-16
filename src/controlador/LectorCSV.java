582586f6a6d42195d2a8e22c51047de5f7ec9806
marc <marc@marc-Aspire-5750G>
2013-12-16 19:59:23 +0100

    LECTURA DE CSV I TRACTAMENT

--- src/controlador/LectorCSV.java
+++ src/controlador/LectorCSV.java
@@ -6,7 +6,13 @@
 
 package controlador;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,7 +20,7 @@ import java.io.File;
  */
 public class LectorCSV {
     
-    private File cvs;
+    private static File cvs;
     
     public LectorCSV(File cvs){
         this.cvs = cvs;
@@ -26,6 +32,32 @@ public class LectorCSV {
 
     public void setCvs(File cvs) {
         this.cvs = cvs;
+    }
+    
+    public static void main(String[] args){
+        try {
+            // Llegir l'arxiu
+            BufferedReader inputStream = new BufferedReader(new FileReader("/home/marc/NetBeansProjects/matriculats.csv"));
+            
+            String linea;
+            while((linea = inputStream.readLine()) != null){
+                String[] sepCometes = linea.split("\"");
+                System.out.println(sepCometes[1]); // CognomsNom
+                System.out.println(sepCometes[3]); // Curs
+                
+                System.out.println(sepCometes[5]); // Materies
+                String[] materies = sepCometes[5].split(",");
+                for(int i = 0; i < materies.length ; i++){
+                    System.out.println(materies[i]);
+                }                
+                
+            }
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(LectorCSV.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(LectorCSV.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
      
 }
